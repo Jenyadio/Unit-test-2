e08e87d9547e10ca84664f39be36aa44cce1e6e7
jenyadio <jenyadio@gmail.com>
2022-03-12 18:12:46 +0300

    lint fixed

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -2,31 +2,30 @@ import sortByHealth from '../app';
 
 test('should sort by health', () => {
   const arr = [
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
   ];
   const result = sortByHealth(arr);
   const expected = [
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-    {name: 'мечник', health: 10},
-  ]
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
   expect(result).toEqual(expected);
 });
 
 test('should sort by health', () => {
   const arr = [
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
   ];
   const result = sortByHealth(arr);
   const expected = [
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-    {name: 'мечник', health: 10},
-  ]
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
   expect(result).not.toBe(expected);
 });
-
