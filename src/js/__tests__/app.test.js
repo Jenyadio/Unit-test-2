2c4c117c262ab8c84e8614e749e31f3a98e17a54
jenyadio <jenyadio@gmail.com>
2022-03-12 18:09:29 +0300

    first

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,32 @@
+import sortByHealth from '../app';
+
+test('should sort by health', () => {
+  const arr = [
+    {name: 'мечник', health: 10},
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+  ];
+  const result = sortByHealth(arr);
+  const expected = [
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+    {name: 'мечник', health: 10},
+  ]
+  expect(result).toEqual(expected);
+});
+
+test('should sort by health', () => {
+  const arr = [
+    {name: 'мечник', health: 10},
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+  ];
+  const result = sortByHealth(arr);
+  const expected = [
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+    {name: 'мечник', health: 10},
+  ]
+  expect(result).not.toBe(expected);
+});
+
